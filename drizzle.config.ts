a418d48c8c9f5ddfd2acb733e9ef7f1589a2934f
chems-eddine24 <obitouchiwa109@gmail.com>
2026-05-13 20:56:04 +0100

    check if the db url is loaded correctly from .env file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from "drizzle-kit";
 
 
 
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+}
+
 export default defineConfig({
-    schema: "./src/db/schema.ts",
+    schema: "./schema.ts",
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
-        url: process.env.DATABASE_URL,
+        url: process.env.DATABASE_URL, // now narrowed to `string`
     },
 });
