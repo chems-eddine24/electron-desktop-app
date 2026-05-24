aca8c2e5d2f57cd30b5f7209a648df7b9ab6fc32
chems-eddine24 <obitouchiwa109@gmail.com>
2026-05-24 11:30:44 +0100

    feat: added IPC const for calling services

--- src/shared/ipc_channels.ts
+++ src/shared/ipc_channels.ts
@@ -3,4 +3,10 @@ export const IPC = {
     TASKS_CREATE:  'tasks:create',
     TASKS_UPDATE:  'tasks:update',
     TASKS_DELETE:  'tasks:delete',
+    PROJECTS_CREATE:  'projects:create',
+    PROJECTS_UPDATE:  'projects:update',
+    PROJECTS_GET_ALL:  'projects:getAll',
+    PROJECTS_DELETE:  'projects:delete',
+    PROJECTS_SELECT:  'projects:select',
+
 } as const
