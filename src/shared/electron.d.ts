125dd55d6e2e1039737f1341c2200a65ce37bb7a
chems-eddine24 <obitouchiwa109@gmail.com>
2026-05-24 11:31:34 +0100

    feat: added window interface

--- src/shared/electron.d.ts
+++ src/shared/electron.d.ts
@@ -1,4 +1,4 @@
-import type { Task } from './types'
+import type { Task, Project } from './types'
 import type { Result } from './ipc-types'
 
 type Status = 'doing' | 'done' | 'paused'
@@ -7,10 +7,18 @@ declare global {
     interface Window {
         electronApi: {
             tasks: {
-                getAll: ()                                                    => Promise<Result<Task[]>>
-                create: (payload: { title: string; description: string; status?: Status }) => Promise<Result<Task>>
-                update: (payload: { task_id: number; title?: string; description?: string; status?: Status }) => Promise<Result<Task>>
-                delete: (id: number)                                          => Promise<Result<void>>
+                getAllTasks: ()                                                    => Promise<Result<Task[]>>
+                createTask: (payload: { title: string; description: string; status?: Status }) => Promise<Result<Task>>
+                updateTask: (payload: { task_id: number; title?: string; description?: string; status?: Status }) => Promise<Result<Task>>
+                deleteTask: (id: number)                                          => Promise<Result<void>>
+            }
+            projects: {
+                getAllProjects: () => Promise<Result<Project[]>>
+                createProject: (payload : {title: string; active: boolean, description: string}) => Promise<Result<Project>>
+                updateProject: (project_id: number, payload : {archive?: boolean; title?: string; active?: boolean, description?: string}) => Promise<Result<Project>>
+                deleteProject: (id: number) => Promise<Result<void>>
+                selectProject: (id: number) => Promise<Result<Project>>
+
             }
         }
     }
