b2201affbb0069059cd0beda9e0903588221e60e
chems-eddine24 <obitouchiwa109@gmail.com>
2026-05-24 11:22:46 +0100

    refactor: removed deadline column and added description column

--- src/db/schema/projects.ts
+++ src/db/schema/projects.ts
@@ -5,12 +5,15 @@ import { pgTable, serial, text, timestamp, boolean } from "drizzle-orm/pg-core";
 export const projects = pgTable("projects", {
     project_id: serial("project_id").primaryKey().notNull(),
     title: text("title").notNull(),
+    description: text("description").notNull(),
     active: boolean("active").notNull(),
     archive: boolean("archive").notNull(),
-    unarchive: boolean("unarchive").notNull(),
     created_at: timestamp("created_at").notNull().defaultNow(),
-    deadline: timestamp("deadline"),
+    updated_at: timestamp("updated_at")
 })
 
 
+
+
 export type Project = typeof projects.$inferSelect;
+export type NewProject = typeof projects.$inferInsert;
