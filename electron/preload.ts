81afbf88c15b533c8949e2f139b84bf53b661620
chems-eddine24 <obitouchiwa109@gmail.com>
2026-05-24 11:12:03 +0100

    feat: exposed project service

--- electron/preload.ts
+++ electron/preload.ts
@@ -3,9 +3,16 @@ import { IPC } from "../src/shared/ipc_channels.ts";
 console.log("Preload loaded")
 contextBridge.exposeInMainWorld('electronApi', {
     tasks: {
-        getAll: ()              => ipcRenderer.invoke(IPC.TASKS_GET_ALL),
-        create: (payload: unknown) => ipcRenderer.invoke(IPC.TASKS_CREATE, payload),
-        update: (payload: unknown) => ipcRenderer.invoke(IPC.TASKS_UPDATE, payload),
-        delete: (title: string)    => ipcRenderer.invoke(IPC.TASKS_DELETE, title),
+        getAllTasks: ()              => ipcRenderer.invoke(IPC.TASKS_GET_ALL),
+        createTask: (payload: unknown) => ipcRenderer.invoke(IPC.TASKS_CREATE, payload),
+        updateTask: (payload: unknown) => ipcRenderer.invoke(IPC.TASKS_UPDATE, payload),
+        deleteTask: (title: string)    => ipcRenderer.invoke(IPC.TASKS_DELETE, title),
+    },
+    projects: {
+        getAllProjects: () => ipcRenderer.invoke(IPC.PROJECTS_GET_ALL),
+        createProject: (payload: unknown) => ipcRenderer.invoke(IPC.PROJECTS_CREATE, payload),
+        updateProject: (project_id: number, payload : {archive?: boolean; title?: string; active?: boolean, description?: string}) => ipcRenderer.invoke(IPC.PROJECTS_UPDATE, project_id, payload),
+        deleteProject: (project_id: number) => ipcRenderer.invoke(IPC.PROJECTS_DELETE, project_id),
+        selectProject: (project_id: number) => ipcRenderer.invoke(IPC.PROJECTS_SELECT, project_id),
     }
 })
