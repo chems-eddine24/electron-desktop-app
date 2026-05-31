c910c001ac7b4e17af202102cae1f3194bed4fa5
chems-eddine24 <obitouchiwa109@gmail.com>
2026-05-31 20:34:05 +0100

    refactor: keep types that handles the ui only

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,24 +1,39 @@
-import {z} from "zod"
-import {tags} from "../db/schema/tags.ts"
-import {projects} from "../db/schema/projects.ts"
-import {tasks} from "../db/schema/tasks.ts"
-import {createTagSchema} from "../shared/schemas/tags.ts"
-import {createProjectSchema, updateProjectSchema} from "../shared/schemas/projects.ts"
-import {createTaskSchema, updateTaskSchema} from "../shared/schemas/tasks.ts"
+export type Project = {
+    project_id: string;
+    title: string;
+    description: string;
+    created_at: string;
+    archive: boolean;
+    active: boolean;
+};
 
-// Project
-export type Project = typeof projects.$inferSelect;
-export type NewProject = typeof projects.$inferInsert;
-export type CreateProjectData = z.infer<typeof createProjectSchema>
-export type UpdateProjectData = z.infer<typeof updateProjectSchema>
-
-// Task
-export type Task           = typeof tasks.$inferSelect
-export type InsertTaskData = typeof tasks.$inferInsert
-export type createTaskData = z.infer<typeof createTaskSchema>
-export type updateTaskData = z.infer<typeof updateTaskSchema>
 export type TaskStatus = 'doing' | 'done' | 'paused'
 
-//Tag
-export type createTagData = z.infer<typeof createTagSchema>
-export type Tag = typeof tags.$inferSelect;
+export type TaskTag = {
+    id: string;
+    name: string;
+};
+
+export type Task = {
+    task_id: string;
+    title: string;
+    description: string;
+    status: TaskStatus;
+    created_at: Date;
+    updated_at: Date | null;
+    project_id?: string;
+};
+
+export type TaskDraft = {
+    title: string;
+    description: string;
+    status: TaskStatus;
+    tag_name: string;
+};
+
+export const emptyDraft: TaskDraft = {
+    title: '',
+    description: '',
+    status: 'doing',
+    tag_name: '',
+};
