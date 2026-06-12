b3ae7eb007066cbd478f674f62aa9c8dea193a54
chems-eddine24 <obitouchiwa109@gmail.com>
2026-06-12 14:28:13 +0100

    refactor: simplify main by delegating wiring to container and extracting ipc registration

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,15 +1,10 @@
 import path from 'path'
 
 import { app, BrowserWindow } from 'electron'
-import { TaskService } from './services/tasks.ts'
-import { TaskRepo } from './repositories/tasks.ts'
-import { ProjectRepo } from './repositories/projects.ts'
-import { ProjectService } from './services/projects.ts'
 import { projectIpcHandlers } from './controllers/projects.ts'
 import { taskIpcHandlers } from './controllers/tasks.ts'
-import { TagRepo } from './repositories/tags.ts'
-import { TagService } from './services/tags.ts'
 import { tagIpcHandlers } from './controllers/tags.ts'
+import {buildContainer} from "../container.ts";
 
 const projectRoot = process.cwd()
 
@@ -36,12 +31,7 @@ function createWindow() {
 
 app.disableHardwareAcceleration()
 app.whenReady().then(() => {
-    const taskRepo = new TaskRepo()
-    const taskService = new TaskService(taskRepo)
-    const projectRepo = new ProjectRepo()
-    const projectService = new ProjectService(projectRepo)
-    const tagRepo = new TagRepo()
-    const tagService = new TagService(tagRepo)
+    const { taskService, projectService, tagService } = buildContainer()
     taskIpcHandlers({ taskService })
     projectIpcHandlers({ projectService })
     tagIpcHandlers({ tagService })
