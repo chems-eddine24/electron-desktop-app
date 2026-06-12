bb7592fbab239f9e4111a1ce39414b431d406eb5
chems-eddine24 <obitouchiwa109@gmail.com>
2026-06-12 11:37:17 +0100

    fix: fixed schema path to ./src/electron/db/schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ if (!process.env.DATABASE_URL) {
 }
 
 export default defineConfig({
-    schema: "./src/db/schema",
+    schema: "./src/electron/db/schema",
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
